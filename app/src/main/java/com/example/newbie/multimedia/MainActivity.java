7db71a7ca9350e4d5ad82664ed3bacce7d6863fd
YogiAdityaS <adityayogi711@gmail.com>
2018-05-20 11:48:15 +0700

    webview

---
+++ app/src/main/java/com/example/newbie/multimedia/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.newbie.multimedia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.webview);
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+
+        //untuk zoom
+        webView.getSettings().setSupportZoom(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.getSettings().setDisplayZoomControls(false);
+
+        //scroll bar
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://roniandarsyah.net/2018/02/28/tugas-sistem-multimedia/");
+    }
+}
